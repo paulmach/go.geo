acc7bae4136ab21178dcd82f3767f4ecf99b23b2
Paul Mach <paulmach@gmail.com>
2014-09-24 12:12:26 -0700

    Add benchmarks to optimize against

---
+++ benchmarks_line_test.go
@@ -0,0 +1,84 @@
+package geo_test
+
+import (
+	"testing"
+
+	geo "."
+)
+
+func BenchmarkLineDistanceFrom(b *testing.B) {
+	l := geo.NewLine(geo.NewPoint(1, 2), geo.NewPoint(3, 4))
+	p := geo.NewPoint(2, 4)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l.DistanceFrom(p)
+	}
+}
+
+func BenchmarkLineSquaredDistanceFrom(b *testing.B) {
+	l := geo.NewLine(geo.NewPoint(1, 2), geo.NewPoint(3, 4))
+	p := geo.NewPoint(2, 4)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l.SquaredDistanceFrom(p)
+	}
+}
+
+func BenchmarkLineProject(b *testing.B) {
+	l := geo.NewLine(geo.NewPoint(1, 2), geo.NewPoint(3, 4))
+	p := geo.NewPoint(2, 4)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l.Project(p)
+	}
+}
+
+func BenchmarkLineMeasure(b *testing.B) {
+	l := geo.NewLine(geo.NewPoint(1, 2), geo.NewPoint(3, 4))
+	p := geo.NewPoint(5, 4)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l.Measure(p)
+	}
+}
+
+func BenchmarkLineInterpolate(b *testing.B) {
+	l := geo.NewLine(geo.NewPoint(1, 2), geo.NewPoint(3, 4))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l.Interpolate(0.5)
+	}
+}
+
+func BenchmarkLineMidpoint(b *testing.B) {
+	l := geo.NewLine(geo.NewPoint(1, 2), geo.NewPoint(3, 4))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l.Midpoint()
+	}
+}
+
+func BenchmarkLineEquals(b *testing.B) {
+	l1 := geo.NewLine(geo.NewPoint(1, 2), geo.NewPoint(3, 4))
+	l2 := geo.NewLine(geo.NewPoint(1, 2), geo.NewPoint(4, 3))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l1.Equals(l2)
+	}
+}
+
+func BenchmarkLineClone(b *testing.B) {
+	l := geo.NewLine(geo.NewPoint(1, 2), geo.NewPoint(3, 4))
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		l.Clone()
+	}
+}
---
+++ benchmarks_point_test.go
@@ -0,0 +1,91 @@
+package geo_test
+
+import (
+	"testing"
+
+	geo "."
+)
+
+func BenchmarkPointDistanceFrom(b *testing.B) {
+	p1 := geo.NewPoint(-122.4167, 37.7833)
+	p2 := geo.NewPoint(37.7833, -122.4167)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p1.DistanceFrom(p2)
+	}
+}
+
+func BenchmarkPointSquaredDistanceFrom(b *testing.B) {
+	p1 := geo.NewPoint(-122.4167, 37.7833)
+	p2 := geo.NewPoint(37.7833, -122.4167)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p1.SquaredDistanceFrom(p2)
+	}
+}
+
+func BenchmarkPointQuadKey(b *testing.B) {
+	p := geo.NewPoint(-122.4167, 37.7833)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.Quadkey(60)
+	}
+}
+
+func BenchmarkPointQuadKeyString(b *testing.B) {
+	p := geo.NewPoint(-122.4167, 37.7833)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.QuadkeyString(60)
+	}
+}
+
+func BenchmarkPointGeoHash(b *testing.B) {
+	p := geo.NewPoint(-122.4167, 37.7833)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.GeoHash()
+	}
+}
+
+func BenchmarkPointGeoHashInt64(b *testing.B) {
+	p := geo.NewPoint(-122.4167, 37.7833)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.GeoHashInt64(60)
+	}
+}
+
+func BenchmarkPointNormalize(b *testing.B) {
+	p := geo.NewPoint(5, 6)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.Normalize()
+	}
+}
+
+func BenchmarkPointEquals(b *testing.B) {
+	p1 := geo.NewPoint(5, 6)
+	p2 := geo.NewPoint(5, 7)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p1.Equals(p2)
+	}
+}
+
+func BenchmarkPointClone(b *testing.B) {
+	p := geo.NewPoint(5, 6)
+
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		p.Clone()
+	}
+}
